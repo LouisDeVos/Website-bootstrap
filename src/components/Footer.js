fcc989ac2236a4ec102edf4209596840c4484b37
LouisDeVos <louis.de.vos.812@gmail.com>
2021-12-02 22:17:50 +0100

    added bootstrap form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className= "d-block">
+        <footer className= "d-block bg-light py-3">
             <p className= "text-center">Copyright 2021 META architecture office</p>
         </footer>
     )
--- src/components/pages/BuildingPhysicsApp.js
+++ src/components/pages/BuildingPhysicsApp.js
@@ -4,9 +4,60 @@ import Footer from '../Footer';
 const BuildingPhysicsApp = () => {
     return (
         <>
-        <div>
-            <p>BuildingPhysicsApp</p>
+        <div class="container">
+            <div class="py-5 text-center">
+                <img class="mb-4 d-block mx-auto" src="https://www.ugent.be/++theme++ugent/static/images/logo_ugent_nl.svg" alt="UGent Logo" width="80"/>
+            <h2>Building Physics Calculator</h2>
+            <p class="lead">Calculate transmission heat loss of your building (EN 12831).</p>
+            </div>
         </div>
+
+        <div class="container">
+            <h4 class="mb-3">General Info</h4>
+            <form novalidate/>
+                <div class="row g-3">
+                    <div class="col-sm-6">
+                        <label for="projectName" class="form-label">
+                            Project Name
+                        </label>
+                        <input id="projectName" type="text" class="form-control" placeholder="My fantastic project" required/>
+                        <div class="invalid-feedback">
+                            Valid Project Name is required.
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="numberOfRooms" class="form-label">
+                            How many rooms are there?
+                        </label>
+                        <input id="numberOfRooms" type="text" class="form-control" placeholder="5" required/>
+                        <div class="invalid-feedback">
+                            Valid number of rooms is required.
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="ExtTemp" class="form-label">
+                            Outdoor temperature
+                        </label>
+                        <input id="ExtTemp" type="text" class="form-control" placeholder="15 °C" required/>
+                        <div class="invalid-feedback">
+                            Valid temperature is required.
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="numberOfRooms" class="form-label">
+                            How many rooms are there?
+                        </label>
+                        <input id="numberOfRooms" type="text" class="form-control" placeholder="5" required/>
+                        <div class="invalid-feedback">
+                            Valid number of rooms is required.
+                        </div>
+                    </div>
+                </div>
+        </div>
+
+       
+
+
         <Footer/>
         </>
     )
