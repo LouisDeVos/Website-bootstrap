a064fee341a1ca2a8c417954e76f1166e488e026
LouisDeVos <louis.de.vos.812@gmail.com>
2021-12-16 14:29:06 +0100

    succesfull API requests

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,31 +11,31 @@ function Home () {
     return (
         <>
         <Carousel fade>
-                <Carousel.Item>
+                <Carousel.Item className="carousel-item">
                     <img
-                        className="d-block w-100"
+                        className="d-block w-100 img-fluid"
                         src={slide1}
                         alt="First slide" />
                     <Carousel.Caption>
                         <h3>LIVING IN THE GREEN</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+
+                <Carousel.Item className="carousel-item">
                     <img
-                        className="d-block w-100"
+                        className="d-block w-100 img-fluid"
                         src={slide2}
                         alt="Second slide" />
-
                     <Carousel.Caption>
                         <h3>KAARLO SARKIAN KATU APARTMENTS</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+
+                <Carousel.Item className="carousel-item">
                     <img
-                        className="d-block w-100"
+                        className="d-block w-100 img-fluid"
                         src={slide3}
                         alt="Third slide" />
-
                     <Carousel.Caption>
                         <h3>THE SURRY</h3>
                     </Carousel.Caption>
