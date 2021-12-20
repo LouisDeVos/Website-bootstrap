d2dce8fae3111dfbf8a9c23bdc8aacf564fae2b6
LouisDeVos <louis.de.vos.812@gmail.com>
2021-12-21 00:40:14 +0100

    finished database

--- src/components/pages/BuildingPhysicsApp.js
+++ src/components/pages/BuildingPhysicsApp.js
@@ -1,110 +1,241 @@
 import React from 'react';
 import Footer from '../Footer';
 import { useState } from 'react';
+import { Form, Col, Row, Button, InputGroup, FormControl } from 'react-bootstrap';
 
 const BuildingPhysicsApp = () => {
-    const [inputs, setInputs] = useState("");
+    const [projectName, setProjectName] = useState(null);
+    const [owner, setOwner] = useState(null);
+    const [Ti, setTi] = useState(null);
+    const [A, setA] = useState(null);
+    const [numberOfLayers, setNumberOfLayers] = useState(null);
+    const [projectCity, setProjectCity] = useState(null);
+    const [transType, setTransType] = useState(null);
+    const [orientation, setOrientation] = useState(null);
+    const [layNumber, setLayNumber] = useState(null);
+    const [layThickness, setLayThickness] = useState(null);
+    const [layMat,setLayMat]= useState(null);
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}))
-    }
+    async function uploadInfo() {
+        var requestOptionsInfo = {
+          method: "PUT",
+          headers: { "Content-Type": 'application/JSON' },
+          body: JSON.stringify({
+            name: projectName,
+            owner: owner,
+            city: projectCity,
+            temp: Ti,
+            surface: A,
+            type: transType,
+            layers: numberOfLayers,
+            orientation: orientation
+          }),
+        };
+    
+        await fetch(
+            `http://localhost:5000/Louis-De-Vos/data/projects/${projectName}/general-info`,
+          requestOptionsInfo);
+      }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(inputs);
-    }
+      async function uploadContruction() {
+        var requestOptionsCity = {
+          method: "PUT",
+          headers: { "Content-Type": 'application/JSON' },
+          body: JSON.stringify({
+            layerNumber: layNumber,  
+            material: layMat,
+            thickness: layThickness
+          }),
+        };
+    
+        await fetch(
+            `http://localhost:5000/Louis-De-Vos/data/projects/${projectName}/construction/${layNumber}`,
+          requestOptionsCity
+        );
+      }  
 
+
+  
     return (
         <>
         <div class="container">
             <div class="py-5 text-center">
               <img class="mb-4 d-block mx-auto" 
               src="http://localhost:5000/Louis-De-Vos/data/images/trisco.png" 
-              alt="Trisco Logo" 
-              width="100"/>
+              alt="Trisco DB Logo" 
+              width="105"/>
             <h2>Building Physics App</h2>
-            <p class="lead">Calculate transmission heat loss of a building part (EN 12831).</p>
+            <p class="lead">Calculate the Transmission Heat Loss through a Building Part (EN 12831)</p>
             </div>
         </div>
+     <div class="container">
+     <div className='m-5'>
+     <h4 className="mb-3">General Info</h4>  
+     <Form noValidate>
+        <Row className="align-items-center">
+        <Col className="mb">
+            <Form.Label>Project Name</Form.Label>
+            <InputGroup className="mb-2">
+            <Form.Control
+              required
+              type="text"
+              onChange={e => setProjectName(e.target.value)}
+              placeholder="My fantastic project"
+            />
+            </InputGroup>
+        </Col>
+        <Col className="mb">
+            <Form.Label>Owner</Form.Label>
+            <InputGroup className="mb-2">
+            <FormControl 
+              id="inlineFormInputGroup" 
+              placeholder="A very lucky owner" 
+              required
+              type="text"
+              onChange={e => setOwner(e.target.value)}
+              />  
+            </InputGroup>
+        </Col>
+        <Col className="mb">
+            <Form.Label>City</Form.Label>
+            <InputGroup className="mb-2">
+            <FormControl 
+              id="inlineFormInputGroup" 
+              placeholder="Gent" 
+              required
+              type="text"
+              onChange={e => setProjectCity(e.target.value)}
+              />  
+            </InputGroup>
+        </Col>
+        <Col className="mb">
+            <Form.Label>Room Temperature</Form.Label>
+            <InputGroup className="mb-2">
+            <FormControl 
+              id="inlineFormInputGroup" 
+              placeholder="20" 
+              required
+              type="number"
+              onChange={e => setTi(e.target.value)}
+              />
+            <InputGroup.Text>°C</InputGroup.Text>  
+            </InputGroup>
+        </Col>
+        </Row>
+        <Row className="align-items-center">
+        <Col className="mb">
+            <Form.Label>Number of Material Layers</Form.Label>
+            <InputGroup className="mb-2">
+            <Form.Control
+              required
+              type="number"
+              onChange={e => setNumberOfLayers(e.target.value)}
+              placeholder="5"
+            />
+            </InputGroup>
+        </Col>
+        <Col className="mb">
+            <Form.Label>Surface of Building Part</Form.Label>
+            <InputGroup className="mb-2">
+            <FormControl 
+              id="inlineFormInputGroup" 
+              placeholder="1" 
+              required
+              type="number"
+              onChange={e => setA(e.target.value)}
+              />
+              <InputGroup.Text>m²</InputGroup.Text>   
+            </InputGroup>
+        </Col>
+        <Col className="mb">
+            <Form.Label>Orientation</Form.Label>
+            <InputGroup className="mb-2">
+            <Form.Select defaultValue="Choose..." onChange={e => setOrientation(e.target.value)}>
+             <option>Choose...</option>
+             <option>Wall</option>
+             <option>Ceiling</option>
+             <option>Floor</option>
+            </Form.Select>
+            </InputGroup>
+        </Col>
+        <Col className="mb">
+            <Form.Label>Type of Transmision</Form.Label>
+            <InputGroup className="mb-2">
+            <Form.Select defaultValue="Choose..." onChange={e => setTransType(e.target.value)}>
+             <option>Choose...</option>
+             <option value="ie">Directly to the exterior</option>
+             <option value="iue">Through unheated space</option>
+             <option value="ig">Through the ground</option>
+             <option value="ij">To or from spaces at different temperatures</option>
+            </Form.Select>
+            </InputGroup>
+        </Col>
+        </Row>
+        <div className="d-grid gap-3">
+        <Button onClick={uploadInfo} className="btn-block" type="submit">Submit</Button>
+        </div> 
+      </Form>
+      </div>
+      
+     
 
-        <div class="container">
-            <h4 class="mb-3">General Info</h4>
-            <form novalidate onSubmit={handleSubmit}>
-                <div class="row g-3">
-                    <div class="col-sm-6">
-                        <label class="form-label">
-                            Project Name
-                        </label>
-                        <input 
-                        name="projectName" 
-                        type="text" 
-                        class="form-control"
-                        value={inputs.projectName || ""}
-                        onChange={handleChange} 
-                        placeholder="My fantastic project" 
-                        required/>
-                        <div class="invalid-feedback">
-                            Valid Project Name is required.
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <label class="form-label">
-                            Room Temperature (in degrees Celsius)
-                        </label>
-                        <input
-                        name="Ti"
-                        type="number"
-                        class="form-control"
-                        value={inputs.Ti || ""}
-                        onChange={handleChange}
-                        placeholder="20 °C"
-                        required/>
-                        <div class="invalid-feedback">
-                            Valid indoor temperature is required.
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <label class="form-label">
-                            Number of material layers
-                        </label>
-                        <input
-                        name="numberOfLayers"
-                        type="number"
-                        class="form-control"
-                        value={inputs.numberOfLayers || ""}
-                        onChange={handleChange}
-                        placeholder="5"
-                        required/>
-                        <div class="invalid-feedback">
-                            Valid number of material layers is required.
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <label class="form-label">
-                            In which Belgian city is the project located?
-                        </label>
-                        <select
-                            value={inputs.Te || ""}
-                            type="number"
-                            onChange={handleChange}
-                            class="form-control"
-                            placeholder="Ghent" 
-                            required>
-                            <option value={-8}>Aalst</option>
-                            <option value={-10}>Aubel</option>
-                            <option value={-8}>Gent</option>
-                        </select>
-                        <div class="invalid-feedback">
-                            Valid city is required.
-                        </div>
-                    </div>
-                    <input type="submit"/>
-                </div>
-            </form>
-            <h3> Your project named "{inputs.projectName}" with an indoor temperature of {inputs.Ti}°C is located in a city with an outdoor temperature of {inputs.Te}.</h3>    
+     <div className='m-5'>
+     <h4 className="mb-3">Construction Properties</h4>
+     <Form noValidate>
+        <Row className="align-items-center">
+        <Col className="mb">
+            <Form.Label>Project Name</Form.Label>
+            <InputGroup className="mb-2">
+            <Form.Control
+              required
+              type="text"
+              onChange={e => setProjectName(e.target.value)}
+              placeholder="My fantastic project"
+            />
+            </InputGroup>
+        </Col>
+        <Col className="mb">
+            <Form.Label>Layer Number</Form.Label>
+            <InputGroup className="mb-2">
+            <Form.Control
+              required
+              type="number"
+              onChange={e => setLayNumber(e.target.value)}
+              placeholder="1"
+            />
+            </InputGroup>
+        </Col>    
+        <Col className="mb">
+            <Form.Label>Material</Form.Label>
+            <InputGroup className="mb-2">
+            <Form.Control
+              required
+              type="text"
+              onChange={e => setLayMat(e.target.value)}
+              placeholder="rock wool"
+            />
+            </InputGroup>
+        </Col>
+        <Col className="mb">
+            <Form.Label>Material Thickness</Form.Label>
+            <InputGroup className="mb-2">
+            <FormControl 
+              id="inlineFormInputGroup" 
+              placeholder="0.20" 
+              required
+              type="number"
+              onChange={e => setLayThickness(e.target.value)}
+              />
+            <InputGroup.Text>m</InputGroup.Text>  
+            </InputGroup>
+        </Col>
+        <div className="d-grid gap-3">
+        <Button onClick={uploadContruction} className="btn-block" type="submit">Submit</Button>
         </div>
+        </Row> 
+      </Form>
+      </div>
+      </div>
 
        
 
